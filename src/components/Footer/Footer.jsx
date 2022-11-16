a3a8b015c0338a25b42cd59e95a13072c8aed813
zfrejn <104075411+zfrejn@users.noreply.github.com>
2022-11-16 18:26:30 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,29 +4,22 @@ import FooterLogo from './FooterLogo'
 const Footer = () => {
     return (
         <footer>
-            {/* <div className='footerWrap'> */}
-                {/* <FooterLogo/> */}
-                <div class="example example_menu">
-                    <div class="all">
-                        <div class="lefter">
-                            <div class="text">Контакты</div>
-                        </div>
-                        <div class="left">
-                            <div class="text">Правила</div>
-                        </div>
-                        <div class="center">
-                            <div class="explainer"><span>о нас</span></div>
-                            <div class="text">Условия</div>
-                        </div>
-                        <div class="right">
-                            <div class="text">Новости</div>
-                        </div>
-                        <div class="righter">
-                            <div class="text">Соглашение</div>
-                        </div>
-                    </div>
+            <div className='footerWrap'>
+                <div className='footerLogo'></div>
+                <ul className='footerNav'>
+                    <li className='footerNavItem'>О нас</li>
+                    <li className='footerNavItem'>Новости</li>
+                    <li className='footerNavItem'>Условия</li>
+                    <li className='footerNavItem'>Соглашение</li>
+                </ul>
+                <div className='footerForm'>
+                    <input className='footerInput' placeholder='Email'></input>
+                    <button className='footerBtn'>Подписаться</button>
                 </div>
-            {/* </div>     */}
+                <div className='companyNameFooter'>
+                    © ООО «САВВАС» 2022
+                </div>
+            </div>
         </footer>
     )
 }
